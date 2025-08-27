71ccbe4faf383075f17b7a7e98ab1c9911df5be6
AgusBrunero <agusbrunero@gmail.com>
2025-08-27 20:14:11 -0300

    Ejemplo de testing

---
+++ tests/AllTest.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "CuTest.h"
+
+CuSuite* testThatFailsGetSuite();
+
+void RunAllTests(void) {
+    CuString *output = CuStringNew();
+    CuSuite* suite = CuSuiteNew();
+
+    CuSuiteAddSuite(suite, testThatFailsGetSuite());
+
+    CuSuiteRun(suite);
+    CuSuiteSummary(suite, output);
+    CuSuiteDetails(suite, output);
+    printf("%s\n", output->buffer);
+}
+
+int main(void) {
+    RunAllTests();
+}
